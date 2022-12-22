bd8f4db3caf2b7c0b94ebec4c4080fa0045d54c1
Alice Fabre-Verdure <alice.fabre-verdure@ensae.fr>
2022-12-22 10:41:32 +0100

    strealit_end

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,3 +35,42 @@ st.markdown( """
 
    </style>   """,   unsafe_allow_html=True
    )
+
+   # Set page title
+st.title('Twitter Sentiment Analysis')
+
+# Load classification model
+with st.spinner('Loading classification model...'):
+    classifier = TextClassifier.load('model-saves/best-model.pt')
+
+import re
+
+allowed_chars = ' AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789~`!@#$%^&*()-=_+[]{}|;:",./<>?'
+punct = '!?,.@#'
+maxlen = 280
+
+def preprocess(text):
+    return ''.join([' ' + char + ' ' if char in punct else char for char in [char for char in re.sub(r'http\S+', 'http', text, flags=re.MULTILINE) if char in allowed_chars]])[:maxlen]
+
+st.subheader('Single tweet classification')
+
+tweet_input = st.text_input('Tweet:')
+
+if tweet_input != '':
+    # Pre-process tweet
+    sentence = Sentence(preprocess(tweet_input))
+
+    # Make predictions
+    with st.spinner('Predicting...'):
+        classifier.predict(sentence)
+
+    # Show predictions
+    label_dict = {'__label__0': 'Negative', '__label__4': 'Positive'}
+
+    if len(sentence.labels) > 0:
+        st.write('Prediction:')
+        st.write(label_dict[sentence.labels[0].value] + ' with ',
+                sentence.labels[0].score*100, '% confidence')
+
+st.caption('Authors : Hugo Favre, Loris Bulliard, Michel Daher Mansour, Alice Fabre-Verdure')
+
