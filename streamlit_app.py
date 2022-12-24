c6b6542a399988fb6cccfe2e9a00e7b989cad2d3
Loris Bulliard <loris.bulliard@ensae.fr>
2022-12-24 13:19:16 +0100

    New changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,5 +71,38 @@ if tweet_input != '':
         st.write(label_dict[sentence.labels[0].value] + ' with ',
                 sentence.labels[0].score*100, '% confidence')
 
+### TWEET SEARCH AND CLASSIFY ###
+st.subheader('Search Twitter for Query')
+
+query = st.text_input('Query:', '#')
+
+if query != '' and query != '#':
+    with st.spinner(f'Searching for and analyzing {query}...'):
+        # initialisation 
+        consumer_key = "neFjQUU9CfE9gFgBK2X4yro2j" # API/Consumer key 
+        consumer_secret = "orhQzemgSx07sMV9lTK197j7ZTXrojnDwdGMQdDA1RUl2Gli5h" # API/Consumer Secret Key
+        access_token = "1204779047252889609-0dCHKl8ZNqCLLmJCBuqP1rULpXYLLY"    # Access token key
+        access_token_secret = "kiY8AIFf4MBOBQGxTbNf3DRCmYYklZT0e87m5SClYvnnX" # Access token Secret key
+
+        # Authentification
+        auth = OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_token_secret)
+        api = tp.API(auth)
+        # Get English tweets from the past 4 weeks
+        no_of_tweets = 100 # change to unlimited 
+
+        #The number of tweets we want to retrieved from the search
+        tweets = api.search_tweets(q=query, count=no_of_tweets)
+        attributes_container = [[tweet.id,tweet.geo,tweet.coordinates, tweet.place, tweet.user.name, tweet.created_at, tweet.favorite_count, tweet.source,  tweet.text, tweet.is_quote_status
+                        , tweet.lang] for tweet in tweets]
+
+        # On def les colonnes du dataframe
+        columns = ["ID","Geo","Coordinates","Place","User", "Date Created", "Number of Likes", "Source of Tweet", "Tweet", "Quoted", "Lang"]
+        #On créé un Df:  
+        tweets_df = pd.DataFrame(attributes_container, columns=columns)
+        tweets_df["Address"]= query
+
+st.write(tweets_df)
+
 st.caption('Authors : Hugo Favre, Loris Bulliard, Michel Daher Mansour, Alice Fabre-Verdure')
 
