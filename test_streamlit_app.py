0408d45a1ad9241ed93f52a0e4266a1d5ef61553
Hugo Favre <hugo.favre@ensae.fr>
2022-12-24 14:50:04 +0100

    test_streamlit_app v1

---
+++ test_streamlit_app.py
@@ -0,0 +1,39 @@
+import pytest # importing our library of testpip install -U pytest
+
+# Import de ma fonction à tester
+from streamlit_app import preprocess
+from flair.data import Sentence
+
+def test_preprocess():
+    
+    text = 'I like christmas'
+
+    sentence = Sentence(preprocess(text))
+
+    assert len(sentence) < 280
+
+def test_preprocess2():
+    
+    text = 'This is a very long sentence with more than 280 characters ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------' 
+
+    sentence = Sentence(preprocess(text))
+
+    assert len(sentence) < 280
+
+
+def test_preprocess3():
+    
+    text = 'This is a question with some allowed punctiations @?.' 
+
+    phrase = Sentence(preprocess(text))
+
+    assert ("@" in str(phrase)) 
+
+
+def test_preprocess4():
+    
+    text = 'This is a question with a disallowed character ¤' 
+
+    phrase = Sentence(preprocess(text))
+
+    assert not("¤" in str(phrase))
